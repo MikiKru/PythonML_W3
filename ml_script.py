3d3e45f480e62be9831fc535629e5946341d68d1
MikiKru <michal_kruczkowski@o2.pl>
2020-04-19 14:44:45 +0200

    ML v6

--- ml_script.py
+++ ml_script.py
@@ -31,11 +31,26 @@ class MLWarmup:
         planets_df2 = planets_df2.dropna(thresh=4)
         print(planets_df2)
         # uzupełnianie pustych danych
-        imp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
-        planets_df3 = imp.fit_transform(planets_df)
+        impFreq = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+        planets_df3 = impFreq.fit_transform(planets_df)
         planets_df3 = DataFrame(planets_df3, columns=list(planets_df.columns))
         print(planets_df3.isnull().sum())
         print(planets_df3)
+        # mapowanie danych jakościowych na liczby porządkowe
+        planets_df4 = planets_df
+        method_mapper = {}
+        for index, num_category in enumerate(planets_df.method.unique()):
+            method_mapper[num_category] = index
+        print(method_mapper)
+        planets_df4['method'] = planets_df['method'].map(method_mapper)
+        # print(planets_df4)
+        # uzupełnienie wartości pustych
+        impMean = SimpleImputer(missing_values=np.nan, strategy='mean')
+        planets_df4 = impMean.fit_transform(planets_df)
+        planets_df4 = DataFrame(planets_df4, columns=list(planets_df.columns))
+        print(planets_df4.isnull().sum())
+        print(planets_df4)
+        # skalowanie danych
 
 ml = MLWarmup()
 # ml.getIrisDataset()
