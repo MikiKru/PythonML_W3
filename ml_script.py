c0fe1d0ea5a2ddbbda9835572f5e64c3c6ff6a89
MikiKru <michal_kruczkowski@o2.pl>
2020-04-19 12:57:52 +0200

    ML v3

--- ml_script.py
+++ ml_script.py
@@ -12,8 +12,18 @@ class MLWarmup:
     def getPlanetsDataset(self):
         planets_df = load_dataset("planets")
         # print(planets_df)
+        column_names = ['method', 'number', 'orbital_period', 'mass', 'distance', 'year']
         # ilość wartości pustych w kolumnach
         print(planets_df.isnull().sum())
+        # usuń wszystkie kolumny zawierające więcej niż połowę wartości pustych - 1035
+        half_dataset_no = int(len(planets_df)/2)
+        planets_df1 = planets_df
+        for c in column_names:
+            if(planets_df[c].isnull().sum() > half_dataset_no):
+                planets_df1 = planets_df1.drop(c,axis=1)
+        print(planets_df1.isnull().sum())
+
+
 
 ml = MLWarmup()
 # ml.getIrisDataset()
