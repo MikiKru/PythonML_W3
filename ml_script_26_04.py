5044ef1a7907dba81c4a7ee91e80d13321f15556
MikiKru <michal_kruczkowski@o2.pl>
2020-04-26 11:58:19 +0200

    ML cont.

--- ml_script_26_04.py
+++ ml_script_26_04.py
@@ -1,6 +1,8 @@
 import seaborn
 import pandas as pd
+from numpy import nan
 from pandas import get_dummies
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -18,8 +20,11 @@ class Classifiers:
         # print(X.describe(include={'boolean'}))
         X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=train_split_percent)
         return X_train, X_test, y_train, y_test
-    def datasetPreprocessing(self, X, columns_to_drop, columns_to_map):
+    def datasetPreprocessing(self, X, columns_to_drop, columns_to_map,
+                             nan_to_median_columns, nan_to_most_freq_columns):
+        # usuwanie
         X_clean = X.drop(columns_to_drop, axis=1)
+        # mapowanie
         for column_name in columns_to_map:
             # konstruowanie mappera
             mapper = {}
@@ -27,8 +32,13 @@ class Classifiers:
                 mapper[category] = index
             # mapowanie
             X_clean[column_name] = X_clean[column_name].map(mapper)
-        # print(X_clean.info())
-        # print(X_clean)
+        # uzupełnianie
+        for column_name in nan_to_median_columns:
+            X_clean[column_name] = X_clean[column_name].fillna(X_clean[column_name].median())
+        # for column_name in nan_to_most_freq_columns:
+        #     impFreq = SimpleImputer(missing_values=nan, strategy='most_frequent')
+        #     X_clean[column_name] = impFreq.fit_transform(X_clean[column_name])
+        print(X_clean.isnull().sum())
     def trainAndTestClassifier(self, clf, X_train, X_test, y_train):
         # trenowanie
         clf.fit(X_train, y_train)
@@ -40,7 +50,10 @@ c = Classifiers()
 c.datasetPreprocessing(
     X = seaborn.load_dataset("titanic").iloc[:, 1:],
     columns_to_drop = ['sex','embarked','class','adult_male','deck','alive'],
-    columns_to_map = ['who','embark_town', 'alone'])
+    columns_to_map = ['who','embark_town', 'alone'],
+    nan_to_median_columns = ['age'],
+    nan_to_most_freq_columns = ['embark_town']
+    )
 # X_train, X_test, y_train, y_test = c.splitDatasetIntoTrainAndTest(
 #     X=seaborn.load_dataset("titanic").iloc[:, 1:],
 #     y=seaborn.load_dataset("titanic")['survived'])
