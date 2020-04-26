4d4b6dfc109d58fe0f45342536957a14d4336abb
MikiKru <michal_kruczkowski@o2.pl>
2020-04-26 10:30:47 +0200

    ml 26.04.2020

--- ml_script_26_04.py
+++ ml_script_26_04.py
@@ -6,15 +6,15 @@ from sklearn.neighbors import KNeighborsClassifier
 
 class Classifiers:
     def splitDatasetIntoTrainAndTest(self, X, y, train_split_percent = 0.6):
-        pd.set_option('display.max_columns', None)
-        print(X)
-        print(X.info())
-        print(X.describe())
-        print(X.describe(include=[pd.np.number]))
-        print(X.describe(include=[pd.np.object]))
-        print(X.describe(include=['category']))
-        print(X.describe(include={'boolean'}))
-        X_train, X_test, y_train, y_test = train_test_split(X,y)
+        # pd.set_option('display.max_columns', None)
+        # print(X)
+        # print(X.info())
+        # print(X.describe())
+        # print(X.describe(include=[pd.np.number]))
+        # print(X.describe(include=[pd.np.object]))
+        # print(X.describe(include=['category']))
+        # print(X.describe(include={'boolean'}))
+        X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=train_split_percent)
         return X_train, X_test, y_train, y_test
 
     def trainAndTestClassifier(self, clf, X_train, X_test, y_train):
@@ -25,9 +25,9 @@ class Classifiers:
         return y_pred
 
 c = Classifiers()
-# for c in seaborn.load_dataset("titanic").columns:
-#     print("'"+c+"'", end=',')
-c.splitDatasetIntoTrainAndTest(
-    X=seaborn.load_dataset("titanic")[{'pclass','sex','age','sibsp','parch','fare','embarked','class','who','adult_male','deck','embark_town','alive','alone'}],
+X_train, X_test, y_train, y_test = c.splitDatasetIntoTrainAndTest(
+    X=seaborn.load_dataset("titanic").iloc[:, 1:],
     y=seaborn.load_dataset("titanic")['survived'] )
+print(X_train)
+print(y_train)
 # c.trainAndTestClassifier(KNeighborsClassifier(n_neighbors=5))
