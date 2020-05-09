7bd92b413bdc578b54894e24d877f3ea16356e28
MikiKru <michal_kruczkowski@o2.pl>
2020-05-09 12:21:14 +0200

    ml 09.05.2020

--- ml_script_09_05.py
+++ ml_script_09_05.py
@@ -1,5 +1,6 @@
 # klasteryzacja
-from sklearn.cluster import KMeans
+from matplotlib import pyplot
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 
@@ -48,9 +49,41 @@ class ClusteringAlgorithms:
         print(y_pred)
         print(y_cls)
         print(accuracy_score(self.iris['target'], y_cls))
+    def getDBSCANN(self, clf):
+        y_pred = clf.fit_predict(self.iris['data'])
+        print(y_pred)
+    def plotRestults(self, cls_list, column1, column2):
+        y_preds = []
+        pyplot.figure()
+        pyplot.subplot(411)
+        pyplot.scatter(self.iris['data'][:,column1], self.iris['data'][:,column2])
+        pyplot.title("Init samples")
+        pyplot.xlabel("x1")
+        pyplot.ylabel("x2")
+        subplot_number = 412
+        for cls_name in cls_list.keys():
+            y_pred = cls_list[cls_name].fit_predict(self.iris['data'])
+            pyplot.subplot(subplot_number)
+            pyplot.scatter(self.iris['data'][:, column1], self.iris['data'][:, column2], c=y_pred)
+            pyplot.title("Clustering: " + cls_name)
+            pyplot.xlabel("x1")
+            pyplot.ylabel("x2")
+            subplot_number += 1
+        pyplot.show()
 
 c = ClusteringAlgorithms()
 c.getIris()
 c.clustering(KMeans(n_clusters=3, init='random'))
-# c.clustering(KMeans(n_clusters=3, init='k-means++'))
+c.clustering(KMeans(n_clusters=3, init='k-means++'))
+c.clustering(AgglomerativeClustering(n_clusters=3, linkage='single'))
+c.clustering(AgglomerativeClustering(n_clusters=3, linkage='single', affinity='manhattan'))
+c.getDBSCANN(DBSCAN())
+c.plotRestults(
+    {"KMeans++" : KMeans(n_clusters=3, init='k-means++'),
+    "AgglomerativeClustering" : AgglomerativeClustering(n_clusters=3, linkage='single'),
+    "DBSCAN" : DBSCAN()},
+    0,1
+)
+
+
 
