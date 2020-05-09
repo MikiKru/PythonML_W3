a36d1553b11b5e0f55d0ddfb3349fd5a9921bf91
MikiKru <michal_kruczkowski@o2.pl>
2020-05-09 11:14:46 +0200

    ml 09.05.2020

--- ml_script_09_05.py
+++ ml_script_09_05.py
@@ -10,7 +10,6 @@ class ClusteringAlgorithms:
         # print(self.iris)
     def clustering(self, cls):
         y_pred = cls.fit_predict(self.iris['data'])
-        print(y_pred)
         clusters = [list(y_pred).index(0), list(y_pred).index(1), list(y_pred).index(2)]
         if(clusters[0] < clusters[1] and clusters[0] < clusters[2]):
             clusters[0] = 0
@@ -37,21 +36,18 @@ class ClusteringAlgorithms:
                 clusters[0] = 2
                 clusters[1] = 1
 
-
         clusters_ordered = sorted(clusters)
-        print(clusters)
-        print(clusters_ordered)
+        # print(clusters)
+        # print(clusters_ordered)
         y_cls = []
         index = 0
         while(index < len(y_pred)):
-            print(y_pred[index])
-            cls_index = clusters.index(y_pred[index])
-            print(cls_index)
-            y_cls.append(clusters_ordered[cls_index])
-
+            cls_index = clusters_ordered.index(y_pred[index])
+            y_cls.append(clusters[cls_index])
             index += 1
+        print(y_pred)
         print(y_cls)
-        # print(accuracy_score(self.iris['target'], y_pred))
+        print(accuracy_score(self.iris['target'], y_cls))
 
 c = ClusteringAlgorithms()
 c.getIris()
